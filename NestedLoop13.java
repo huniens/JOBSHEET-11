25b3b3fef1f2963ba046e66ed78347ae0a52f56b
hunien <huniens06@gmail.com>
2023-12-10 12:27:34 +0700

    Modify average value for each city

--- NestedLoop13.java
+++ NestedLoop13.java
@@ -9,19 +9,30 @@ public class NestedLoop13 {
         Scanner input13 = new Scanner(System.in);
         double[][] temps = new double[5][7];
 
-        for (int i = 0; i < temps.length; i++) {
+         // Input temperatures for each city and day
+         for (int i = 0; i < temps.length; i++) {
             System.out.println("City: " + i);
             for (int j = 0; j < temps[0].length; j++) {
-                System.out.print("Day " + (j + 1)+": ");
+                System.out.print("Day " + (j + 1) + ": ");
                 temps[i][j] = input13.nextDouble();
             }
             System.out.println();
         }
+
+        // Display temperatures and calculate average for each city
         for (int i = 0; i < temps.length; i++) {
             System.out.println("City: " + i);
-            for (double temperature : temps[i]) {
-                System.out.print(temperature + " ");
+            double cityTotal = 0; // Variable to store the sum of temperatures for the city
+
+            // Display temperatures for each day and calculate the sum
+            for (int j = 0; j < temps[0].length; j++) {
+                System.out.print(temps[i][j] + " ");
+                cityTotal += temps[i][j];
             }
+
+            // Calculate and display the average for the city
+            double cityAverage = cityTotal / temps[0].length;
+            System.out.println("\nAverage Temperature: " + cityAverage);
             System.out.println();
         }
     }
